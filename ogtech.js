f5e6cd88a4078e54d4dc88ef9c844aa7a554d7c7
bright <ghena.bright@gmail.com>
2022-07-14 12:51:21 +0100

    first commit

---
+++ ogtech.js
@@ -0,0 +1,7 @@
+let yourName = prompt("what is your name?");
+let theirName = prompt("what is their name?");
+
+let loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) ;
+
+alert("The Love Score for " + yourName + " and " + theirName + " = " + "  "+ loveScore + "%");
